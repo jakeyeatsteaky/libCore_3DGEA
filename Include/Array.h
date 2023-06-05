50bfe9623bdfc4ec8150af4f76db9042d44e7546
Jake Baker <jakebaker@Jakes-MBP.attlocal.net>
2023-06-05 07:27:14 -0700

    Test Append and SetElement

--- Include/Array.h
+++ Include/Array.h
@@ -46,7 +46,7 @@ public:
         delete[] m_memBlock;
     }
 
-    Array& operator=(const Array& other) {
+    inline Array& operator=(const Array& other) {
         if (&other == this)
             return *this;
         
@@ -64,7 +64,7 @@ public:
         return *this;
     }
 
-    Array& operator=(Array&& other) noexcept {
+    inline Array& operator=(Array&& other) noexcept {
         if(this != &other) {
             m_memBlock = std::exchange(other.m_memBlock, nullptr);
             m_quantity = std::exchange(other.m_quantity, 0);
@@ -79,9 +79,10 @@ public:
     const T* Data() const;
     T& operator[] (size_t idx);
     const T& operator[] (size_t idx) const;
-    void Append (const T& element);
+    void Append(const T& element);
     void SetElement(size_t idx, const T& element);
-
+    size_t GetGrowBy() const;
+    size_t GetMaxQuantity() const;    
 
 private:
     T* m_memBlock;
@@ -89,6 +90,127 @@ private:
     size_t m_maxQuantity;
     size_t m_growBy;
 };
+
+
+template <typename T>
+inline size_t Array<T>::Size() const
+{
+    return m_quantity;
+}
+
+template <typename T>
+inline T* Array<T>::Data()
+{
+    return m_memBlock;
+}
+
+template <typename T>
+inline const T* Array<T>::Data() const
+{
+    return m_memBlock;
+}
+
+template <typename T>
+inline T& Array<T>::operator[] (size_t idx)
+{
+    #ifdef DEBUG
+        if(idx > m_maxQuantity - 1 || idx < 0) 
+            throw std::out_of_range("Array index out of bounds\n");
+    #else 
+        if(idx > m_maxQuantity) 
+            idx = m_maxQuantity;
+        if(idx < 0) 
+            idx = 0;
+    #endif 
+        return m_memBlock[idx]; 
+}
+
+template <typename T>
+inline const T& Array<T>::operator[] (size_t idx) const
+{
+    #ifdef DEBUG
+        if(idx > m_maxQuantity - 1 || idx < 0) 
+            throw std::out_of_range("Array index out of bounds\n");
+    #else 
+        if(idx > m_maxQuantity) 
+            idx = m_maxQuantity;
+        if(idx < 0) 
+            idx = 0;
+    #endif 
+        return m_memBlock[idx]; 
+}
+
+template <typename T>
+inline void Array<T>::Append (const T& element)
+{
+    // Add element to array at position = quantity, and will increment quantity by 1
+    // If the memory block for this array is full, it will dynamically resize to accomodate the new element
+    if(m_quantity == m_maxQuantity)
+    {
+        size_t newSize = m_quantity + m_growBy;
+        T* tempBlock = new (std::nothrow) T[newSize]; //std::nothrow returns nullptr if resize allocation fails
+        if(tempBlock)
+        {
+            for(size_t i = 0; i < m_quantity; ++i) {
+            #if 0
+                // Deep copy of the data in the existing array - less efficient
+                // There is support for move semantics so we're gonna use that
+                // T needs to support move semantics for this to work,
+                // Automatic fail state will be to perform a deep copy so additional
+                // error checking is not necessary
+                tempBlock[i] = m_memBlock[i];
+            #else
+                tempBlock[i] = std::move(m_memBlock[i]);
+            #endif
+            }
+            delete[] m_memBlock;
+            m_memBlock = tempBlock;
+            m_maxQuantity = newSize;
+        }
+        else {
+            throw std::runtime_error("Memory Allocation Failed: Array::Append()\n");
+        }
+    }
+    m_memBlock[m_quantity] = element;
+    m_quantity++;
+}
+
+template <typename T>
+inline void Array<T>::SetElement(size_t idx, const T& element)
+{
+    // Set element at any index.  If the index is outside the size of the array, resize so that idx = newQuantity-1
+    if(idx > m_maxQuantity) {
+        size_t newSize = idx + 1;
+        T* tempBlock = new (std::nothrow) T[newSize];
+        if(tempBlock) {
+            for(size_t i = 0; i < m_maxQuantity; ++i) {
+                tempBlock[i] = std::move(m_memBlock[i]);
+            }
+            delete[] m_memBlock;
+            m_memBlock = tempBlock;
+            m_maxQuantity = newSize;
+            m_quantity++;// SetElement only increments the Size of the array if the index of the element being set is bigger than th current size
+        }
+        else {
+            throw std::runtime_error("Memory Allocation failed: Array::SetElement(size_t, const T&)\n");
+        }
+    }
+    else {
+        m_memBlock[idx] = element;
+    }
+}
+
+template <typename T>
+inline size_t Array<T>::GetGrowBy() const
+{
+    return m_growBy;
+}
+
+template <typename T>
+inline size_t Array<T>::GetMaxQuantity() const
+{
+    return m_maxQuantity;
+}
 
 
 
--- Src/TEST.cpp
+++ Src/TEST.cpp
@@ -25,18 +25,60 @@ void TEST::assertTrue(const T& condition) {
 namespace TestCases 
 {
     void TestArray() {
-        Array<int> array1(5,5);
-        array1.SetElement(0, 0);
-        array1.SetElement(1, 1);
-        array1.SetElement(2, 2);
-        array1.SetElement(3, 3);
-        array1.SetElement(4, 4);
+        {
+            // Test Set Element
+            Array<int> array1(5,5);
+            array1.SetElement(0, 0);
+            array1.SetElement(1, 1);
+            array1.SetElement(2, 2);
+            array1.SetElement(3, 3);
+            array1.SetElement(4, 4);
 
-        TEST test1("Test Array::SetElement()");
-        test1.assertEquals(0,array1[0]);
-        test1.assertEquals(1,array1[1]);
-        test1.assertEquals(2,array1[2]);
-        test1.assertEquals(3,array1[3]);
-        test1.assertEquals(4,array1[4]);
+            TEST test1("Test Array::SetElement()");
+            test1.assertEquals<int>(0,array1[0]);
+            test1.assertEquals<int>(1,array1[1]);
+            test1.assertEquals<int>(2,array1[2]);
+            test1.assertEquals<int>(3,array1[3]);
+            test1.assertEquals<int>(4,array1[4]);
+        }
+
+        {
+            // Test GrowBy
+            Array<float> array2(1,10);
+            TEST test2("Test Array::Append()");
+            TEST test3("Test Array::Size()");
+            TEST test4("Test Array::GetMaxQuantity()");
+
+            array2.SetElement(0, 1.0); 
+            test2.assertEquals<float>(1.0, array2[0]);
+            test3.assertEquals<size_t>(1, array2.Size());
+            test4.assertEquals<size_t>(1, array2.GetMaxQuantity());
+
+            array2.Append(2.0); 
+            test2.assertEquals<float>(2.0, array2[1]);
+            test3.assertEquals<size_t>(2, array2.Size());
+            test4.assertEquals<size_t>(11, array2.GetMaxQuantity());
+
+            array2.Append(3.0); 
+            test2.assertEquals<float>(3.0, array2[2]);
+            test3.assertEquals<size_t>(3, array2.Size());
+            test4.assertEquals<size_t>(11, array2.GetMaxQuantity());
+
+            array2.Append(4.0); 
+            test2.assertEquals<float>(4.0, array2[3]);
+            test3.assertEquals<size_t>(4, array2.Size());
+            test4.assertEquals<size_t>(11, array2.GetMaxQuantity());
+
+            array2.Append(5.0); 
+            test2.assertEquals<float>(5.0, array2[4]);
+            test3.assertEquals<size_t>(5, array2.Size());
+            test4.assertEquals<size_t>(11, array2.GetMaxQuantity());
+
+            array2.Append(6.0);
+            test2.assertEquals<float>(6.0, array2[5]);
+            test3.assertEquals<size_t>(6, array2.Size());
+            test4.assertEquals<size_t>(11, array2.GetMaxQuantity());
+        }
+
     }
 }
