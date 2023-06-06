8e2cf97be8f1e37475a695f04c3c79a1eb5e87c9
Jake Baker <jakebaker@Jakes-MacBook-Pro.local>
2023-06-06 07:42:26 -0700

    notes for tomorromw

--- Include/ArrayExperiment.h
+++ Include/ArrayExperiment.h
@@ -2,39 +2,25 @@
 #define ARRAY_EXPERIMENT_H
 
 #include <iostream>
-#if 0 // =========== GLOBAL TO DO =============
 
-    1) [x] implement Append method 
-    2) [x] implement SetElement method
-    3) [x] Create TEST Class and test cases from append and setelement
-    4) [x] Test Append
-    5) [x] Test SetElement -
-    6) [ ] Consider if SetElement works the way I want it to --> Is there a better way to create an array and preinitialize the values, versus creating the container, then individually setting each index to be a specific value.  This is how SetElement works and I'm not sure if thats a good' way of doing things.
-    7)      [ ] Create "element set" bit field to be able to correctly increment count with set element
-    8) [ ] Continue on makin the rest of the Array Class
-
-    ...) [ ] Start documenting some of the things you learn on a website
-                --> squarespace?  github pages?  
-                --> Non-trivial things that you did not expect would be worthwhile
-                    to mention: template issue with .cpp file, TEST class, Array method things and considerations
-
-    --> write your own implementation, then confer with GenQuest to see if theres anything to add
-    --> Debug and release mode error handling.
-
-#endif
 
 template <typename T>
-class Array
+class Array_Ex
 {
 public:
 
     struct Element {
-        T value;
-        bool empty;
+        T value = 0;
+        bool empty = true;
 
         Element(){
             new (&value) T();
             empty = true;
+        }
+
+        Element(const T& data) {
+            value = data;
+            empty = false;
         }
 
         ~Element() {
@@ -44,15 +30,15 @@ public:
         }
     };
 
-    inline Array(size_t size = 1, size_t growBy = 1): m_count(0), m_size(size), m_growBy(growBy) {
+    inline Array_Ex(size_t size = 1, size_t growBy = 1): m_count(0), m_size(size), m_growBy(growBy) {
         m_memBlock = new Element[m_size]; // "new" operator calls default constructors for T.  Using malloc requires for loop of 'placement new' to initialize elements with default constructor
     }
 
-    inline QuickAdd(size_t idx, const T& element) {
-        m_memBlock[idx] = Element(std::move(element))
+    inline void QuickAdd(size_t idx, const T& elementData) {
+        m_memBlock[idx] = std::move(Element(elementData));
     }
 
-    inline Array(const Array& other): m_count(other.m_count), m_size(other.m_size), m_growBy(other.m_growBy) {
+    inline Array_Ex(const Array_Ex& other): m_count(other.m_count), m_size(other.m_size), m_growBy(other.m_growBy) {
         
         m_memBlock = new Element[m_size];
         for(size_t i = 0; i < m_size; ++i) {
@@ -60,7 +46,7 @@ public:
         }        
     }
 
-    inline Array(Array&& other) noexcept :
+    inline Array_Ex(Array_Ex&& other) noexcept :
         m_memBlock(other.m_memBlock),
         m_count(other.m_count),
         m_size(other.m_size),
@@ -70,11 +56,11 @@ public:
             other.m_size = 0;
         }
 
-    inline ~Array() {
+    inline ~Array_Ex() {
         delete[] m_memBlock;
     }
 
-    inline Array& operator=(const Array& other) {
+    inline Array_Ex& operator=(const Array_Ex& other) {
         if (&other == this)
             return *this;
         
@@ -92,7 +78,7 @@ public:
         return *this;
     }
 
-    inline Array& operator=(Array&& other) noexcept {
+    inline Array_Ex& operator=(Array_Ex&& other) noexcept {
         if(this != &other) {
             m_memBlock = std::exchange(other.m_memBlock, nullptr);
             m_count = std::exchange(other.m_count, 0);
@@ -121,41 +107,41 @@ private:
 
 
 template <typename T>
-inline size_t Array<T>::Size() const
+inline size_t Array_Ex<T>::Size() const
 {
     return m_size;
 }
 
 template <typename T>
-inline T* Array<T>::Data()
+inline T* Array_Ex<T>::Data()
 {
     return m_memBlock;
 }
 
 template <typename T>
-inline const T* Array<T>::Data() const
+inline const T* Array_Ex<T>::Data() const
 {
     return m_memBlock;
 }
 
 template <typename T>
-inline const size_t Array<T>::GetCount() const
+inline const size_t Array_Ex<T>::GetCount() const
 {
     return m_count;
 }
 
 template <typename T>
-inline const size_t Array<T>::GetGrowBy() const
+inline const size_t Array_Ex<T>::GetGrowBy() const
 {
     return m_growBy;
 }
 
 template <typename T>
-inline T& Array<T>::operator[] (size_t idx)
+inline T& Array_Ex<T>::operator[] (size_t idx)
 {
     #ifdef DEBUG
         if(idx > m_size - 1 || idx < 0) 
-            throw std::out_of_range("Array index out of bounds\n");
+            throw std::out_of_range("Array_Ex index out of bounds\n");
     #else 
         if(idx > m_size) 
             idx = m_size;
@@ -166,11 +152,11 @@ inline T& Array<T>::operator[] (size_t idx)
 }
 
 template <typename T>
-inline const T& Array<T>::operator[] (size_t idx) const
+inline const T& Array_Ex<T>::operator[] (size_t idx) const
 {
     #ifdef DEBUG
         if(idx > m_size - 1 || idx < 0) 
-            throw std::out_of_range("Array index out of bounds\n");
+            throw std::out_of_range("Array_Ex index out of bounds\n");
     #else 
         if(idx > m_size) 
             idx = m_size;
@@ -181,10 +167,10 @@ inline const T& Array<T>::operator[] (size_t idx) const
 }
 
 template <typename T>
-inline void Array<T>::Append (const T& element)
+inline void Array_Ex<T>::Append (const T& element)
 {
-    // Add element to array at position = quantity, and will increment quantity by 1
-    // If the memory block for this array is full, it will dynamically resize to accomodate the new element
+    // Add element to Array_Ex at position = quantity, and will increment quantity by 1
+    // If the memory block for this Array_Ex is full, it will dynamically resize to accomodate the new element
     if(m_count == m_size)
     {
         size_t newSize = m_count + m_growBy;
@@ -193,7 +179,7 @@ inline void Array<T>::Append (const T& element)
         {
             for(size_t i = 0; i < m_count; ++i) {
             #if 0
-                // Deep copy of the data in the existing array - less efficient
+                // Deep copy of the data in the existing Array_Ex - less efficient
                 // There is support for move semantics so we're gonna use that
                 // T needs to support move semantics for this to work,
                 // Automatic fail state will be to perform a deep copy so additional
@@ -208,7 +194,7 @@ inline void Array<T>::Append (const T& element)
             m_size = newSize;
         }
         else {
-            throw std::runtime_error("Memory Allocation Failed: Array::Append()\n");
+            throw std::runtime_error("Memory Allocation Failed: Array_Ex::Append()\n");
         }
     }
     m_memBlock[m_count] = element;
@@ -216,9 +202,9 @@ inline void Array<T>::Append (const T& element)
 }
 
 template <typename T>
-inline void Array<T>::SetElement(size_t idx, const T& element)
+inline void Array_Ex<T>::SetElement(size_t idx, const T& element)
 {
-    // Set element at any index.  If the index is outside the size of the array, resize so that idx = newQuantity-1
+    // Set element at any index.  If the index is outside the size of the Array_Ex, resize so that idx = newQuantity-1
     if(idx > m_size) {
         size_t newSize = idx + 1;
         T* tempBlock = new (std::nothrow) T[newSize];
@@ -229,10 +215,10 @@ inline void Array<T>::SetElement(size_t idx, const T& element)
             delete[] m_memBlock;
             m_memBlock = tempBlock;
             m_size = newSize;
-            m_count++;// SetElement only increments the Size of the array if the index of the element being set is bigger than th current size
+            m_count++;// SetElement only increments the Size of the Array_Ex if the index of the element being set is bigger than th current size
         }
         else {
-            throw std::runtime_error("Memory Allocation failed: Array::SetElement(size_t, const T&)\n");
+            throw std::runtime_error("Memory Allocation failed: Array_Ex::SetElement(size_t, const T&)\n");
         }
     }
     else {
--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 #include "Array.h"
-
+#include "ArrayExperiment.h"
 #include "TEST.h"   
-
-struct Object
-{
-    int a;
-    int b;
-    float c;
-    union {
-        int aa;
-        float bb;
-    };
-};
-
 
 int main(int argc, char** argv)
 {
-    TestCases::TestArray(); 
-    Array<int> array(5,5);
-    Array<int> array1 = array;
-    std::cout << array.Data() << " " << array.Size() << std::endl;
+    //TestCases::TestArray(); 
+    //Array<int> array(5,5);
+    //Array<int> array1 = array;
+    //std::cout << array.Data() << " " << array.Size() << std::endl;
+
+    Array_Ex<int> array1(5,5);
+    array1.QuickAdd(0, 1);
+    array1.QuickAdd(1, 2);
+    array1.QuickAdd(2, 3);
+    array1.QuickAdd(3, 4);
+
     return 0;
 }
